0324d86c207be1937d7426d4510295f407a39598
Mike Hummel <mh@mhus.de>
2023-01-11 08:52:12 +0100

    add auto update and fixes

---
+++ backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. ../rpi-backup-config.sh
+
+git pull
+
+NAME=${NAME:-$HOSTNAME}
+FILE=$NAME-$(date +"%Y%m%d")-img.gz
+
+t=$(smbclient -U $USER $SMB_MOUNT -c "cd /$TARGET/;ls"|grep $NAME-|cut -d \- -f 2)
+ismonth=$(echo $t| grep -c ^$(date +"%Y%m"))
+if [ -z "$t" -o "$ismonth" == 0 ]; then
+  echo "Backup not found"
+  echo $t
+else
+  echo "Backup already done"
+  echo $t
+  exit 0  
+fi
+
+removeFile() {
+  echo "Remove File"
+  sleep 30
+  smbclient -U $USER $SMB_MOUNT -c "cd /$TARGET/;del $FILE"   
+}
+
+echo $(date +"%Y-%m-%d %H-%M-%S") Backup $FILE
+dd if=/dev/mmcblk0 bs=100M | gzip - | smbclient -U $USER --max-protocol=SMB3 -b 1024000 $SMB_MOUNT -c "cd /$TARGET/;put - $FILE" || removeFile
+echo $(date +"%Y-%m-%d %H-%M-%S") Finished
--- cron.sh
+++ cron.sh
@@ -2,17 +2,6 @@
 cd "$(dirname "$0")"
 . ../rpi-backup-config.sh
 
-NAME=${NAME:-$HOSTNAME}
-FILE=$NAME-$(date +"%Y%m%d")-img.gz
+git pull
 
-t=$(smbclient -U $USER $SMB_MOUNT -c "cd /$TARGET/;ls"|grep $NAME-|cut -d \- -f 2)
-doit=0
-ismonth=$(echo $t| grep -c ^$(date +"%Y%m"))
-if [ -z "$t" -o $ismonth > 0 ]; then
-  doit=1
-fi
-let s=$RANDOM%20*100
-echo Sleep $s
-sleep $s
-echo Backup $FILE
-dd if=/dev/mmcblk0 bs=100M | gzip - | smbclient -U $USER --max-protocol=SMB3 -b 1024000 $SMB_MOUNT -c "cd /$TARGET/;put - $FILE"
+./backup.sh
